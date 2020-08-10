cb6bde9272b2f89fc935948fadd1d9c809485f05
metinozkn <metin.ozkn@hotmail.com>
2020-08-10 17:11:54 +0300

    completed simple component section

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ["../src/stories/**/*.stories.js"],
-  addons: ["@storybook/addon-actions", "@storybook/addon-links"],
+  addons: [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+  ],
 };
